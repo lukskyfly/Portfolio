5b27adc5a8798a808f911f12f6825e14e2ad61e2
lukskyfly <lukasz.grazda@gmail.com>
2023-04-14 22:41:57 +0200

    added js activ link

--- script/script.js
+++ script/script.js
@@ -1,23 +1,22 @@
-
 //zaznaczenie w navigacji aktywnej secji
 let sections = document.querySelectorAll('section')
 let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
 	sections.forEach(sec => {
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute('id');
+		let top = window.scrollY
+		let offset = sec.offsetTop - 150
+		let height = sec.offsetHeight
+		let id = sec.getAttribute('id')
 
 		if (top >= offset && top < offset + height) {
 			navLinks.forEach(links => {
 				links.classList.remove('active')
-				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-			});
-		};
-	});
-};
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+			})
+		}
+	})
+}
 // dodawanie klasy .hide (przenoszenie na gore strony)
 const arrowUp = document.querySelector('.scrollToTop')
 function trackScroll() {
