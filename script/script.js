d57981485ca8ca6185dd97477489d368a82b8366
lukskyfly <lukasz.grazda@gmail.com>
2023-05-05 12:23:40 +0200

    added fix style

--- script/script.js
+++ script/script.js
@@ -62,7 +62,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger'],
+    strings: ['Frontend Developer', 'Technical Programmatic', 'Blogger'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
